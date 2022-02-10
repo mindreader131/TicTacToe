c457d7464814445a4b667a2416c9b12f902b944e
MichaelZigray <59930528+MichaelZigray@users.noreply.github.com>
2022-02-10 13:59:03 -0700

    PlaceMarker function added

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -10,10 +10,32 @@ std::vector<std::vector<char>> CreateBoard() {
 }
 
 void DisplayBoard(std::vector<std::vector<char>> board) {
+    std::cout<<"-------------"<<std::endl;
     for(int i = 0; i < board.size(); i++) {
+        std::cout << "| ";
         for(int j = 0; j < board[i].size(); j++) {
-            std::cout << board[i][j] << " ";
+            std::cout << board[i][j] << " | ";
         }
         std::cout << std::endl;
+        std::cout<<"-------------"<<std::endl;
+        
     }
+}
+
+void PlaceMarker(std::vector<int> location, std::vector<std::vector<char>> & board, bool isX) {
+    char XorO = isX ? 'X' : 'O';
+    board[location[0]][location[1]] = XorO;
+}
+
+int main() {
+    std::vector<std::vector<char>> board = CreateBoard();
+    DisplayBoard(board);
+    std::vector<int> location = {1,2};
+    PlaceMarker(location, board, true);
+    std::cout<<std::endl;
+    DisplayBoard(board);
+    location = {0,0};
+    PlaceMarker(location, board, false);
+    std::cout<<std::endl;
+    DisplayBoard(board);
 }
