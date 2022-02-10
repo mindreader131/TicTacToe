869bb1e4d83281a04cffb90f851fc5a7fbcd0a97
MichaelZigray <59930528+MichaelZigray@users.noreply.github.com>
2022-02-10 13:20:57 -0700

    CreateBoard function

---
+++ TicTacToe.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+std::vector<std::vector<char>> CreateBoard() {
+    std::vector<std::vector<char>> output = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}};
+    return (output);
+}
