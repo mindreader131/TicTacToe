f1f9a760b9c6f1420275246d01b9cb64867ddae3
MichaelZigray <59930528+MichaelZigray@users.noreply.github.com>
2022-02-10 13:37:37 -0700

    Added DisplayBoard function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 std::vector<std::vector<char>> CreateBoard() {
     std::vector<std::vector<char>> output = {
@@ -6,4 +7,13 @@ std::vector<std::vector<char>> CreateBoard() {
         {' ', ' ', ' '},
         {' ', ' ', ' '}};
     return (output);
-} 
+}
+
+void DisplayBoard(std::vector<std::vector<char>> board) {
+    for(int i = 0; i < board.size(); i++) {
+        for(int j = 0; j < board[i].size(); j++) {
+            std::cout << board[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
